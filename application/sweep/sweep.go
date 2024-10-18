ce3014853655adf8dd55bddda770cf09a2b4e929
Liam Conrad <liam@w4pho.net>
2024-10-18 14:55:30 -0500

    set up flags

--- application/sweep/sweep.go
+++ application/sweep/sweep.go
@@ -10,6 +10,24 @@ import (
 
 const Version = "0.0.1"
 
+func init() {
+	cmdRoot.Flags().IntVarP(&flagSweepCount, "count", "c", -1, "Number of sweeps (Default: continuous)")
+	cmdRoot.Flags().Float64VarP(&flagFrequencyStart, "start", "s", 1000.0, "Start frequency (Hz)")
+	cmdRoot.Flags().Float64VarP(&flagFrequencyEnd, "end", "e", 10000.0, "End frequency (Hz)")
+	cmdRoot.Flags().BoolVarP(&flagLogarithmic, "log", "l", true, "Logarithmic sweep (default: true)")
+	cmdRoot.Flags().BoolVarP(&flagLinear, "lin", "L", false, "Linear sweep (default: false)")
+
+	cmdRoot.MarkFlagsMutuallyExclusive("logarithmic", "linear")
+}
+
+var (
+	flagSweepCount     int
+	flagFrequencyStart float64
+	flagFrequencyEnd   float64
+	flagLogarithmic    bool
+	flagLinear         bool
+)
+
 var cmdRoot = &cobra.Command{
 	Use:     "sweep",
 	Short:   "sweep",
