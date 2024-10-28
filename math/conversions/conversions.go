004154ded05c022166fb09e07fcf44da5ac25463
Liam Conrad <liam@w4pho.net>
2024-10-28 08:17:07 -0500

    add fns for dBV and dBu

--- math/conversions/conversions.go
+++ math/conversions/conversions.go
@@ -1,6 +1,8 @@
 package conversions
 
-import "math"
+import (
+	"math"
+)
 
 const (
 	ref1V   float64 = 1
@@ -11,11 +13,19 @@ func Vpp2Vrms(vpp float64) float64 {
 	return vpp / (2 * math.Sqrt(2))
 }
 
+func Vpp2dBV(vpp float64) float64 {
+	return Vrms2dBV(Vpp2Vrms(vpp))
+}
+
+func Vpp2dBu(vpp float64) float64 {
+	return Vrms2dBu(Vpp2Vrms(vpp))
+}
+
 func Vrms2dBV(vrms float64) float64 {
 	return v2dB(vrms, ref1V)
 }
 
-func Vrms2dBU(vrms float64) float64 {
+func Vrms2dBu(vrms float64) float64 {
 	return v2dB(vrms, ref0775)
 }
 
